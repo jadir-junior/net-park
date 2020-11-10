8d8954c748f9f2a7b7493eacc4e4fc26ab1aa64d
Jadir Junior <jadirjsjunior@gmail.com>
2020-11-10 09:19:28 -0300

    testes green

--- src/app/components/extract/extract.component.spec.ts
+++ src/app/components/extract/extract.component.spec.ts
@@ -9,12 +9,20 @@ describe('ExtractComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExtractComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ExtractComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ExtractComponent);
     component = fixture.componentInstance;
+    component.extract = {
+      valor: '29.90',
+      garagem: 'Paulist',
+      codGaragem: '111',
+      dataEntrada: '2020-03-25T12:00:00Z',
+      dataSaida: '2020-03-25T13:00:00Z',
+      endereco: 'AV PAULISTA 900',
+    };
     fixture.detectChanges();
   }));
 
